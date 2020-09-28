42443b2390a3c26a55b5e7935ef9cd46c4a92174
allive <a.ivanov@2050.digital>
2020-09-28 19:43:18 +0300

    web app only http and port 90 for nginx

--- bin/www.js
+++ bin/www.js
@@ -2,22 +2,23 @@
 import app from '../flutterServer.js'
 import fs from 'fs'
 import http from 'http';
-import https from 'https'
+/*import https from 'https'
 try{
     var privateKey  = fs.readFileSync(process.env.PRIVATE_KEY_SSL, 'utf8');
     var certificate = fs.readFileSync(process.env.CERT_SSL, 'utf8');
 }catch(e){}
 var credentials = {key: privateKey, cert: certificate};
-
+*/
 
 function startServer() {
-    if(typeof credentials.key || typeof credentials.cer =='undefined'){
+    //if(typeof credentials.key || typeof credentials.cer =='undefined'){
         var httpServer = http.createServer(app);
-        httpServer.listen(80);
-    }else{
+        httpServer.listen(90);
+    /*}else{
         var httpsServer = https.createServer(credentials, app);
         httpsServer.listen(443);
     }
+    */
  }
 
 
