8a4ea2c8a3b07e1d7089e4399007acffae721dcb
Andrey Evdokimov <a.evdokimov@2050.digital>
2020-09-28 14:36:45 +0300

    [front] implemented expandable search bar

--- public-flutter/flutter_service_worker.js
+++ public-flutter/flutter_service_worker.js
@@ -12,9 +12,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "3cb4ae3eb2daef4a0c7a13b49ec3a0c5",
-"/": "3cb4ae3eb2daef4a0c7a13b49ec3a0c5",
-"main.dart.js": "a50b7350e07488a363e98658d9a574e8",
+"index.html": "89c199347f171845fcb2a358fa59acf1",
+"/": "89c199347f171845fcb2a358fa59acf1",
+"main.dart.js": "124bd4125c3bf4f8be6d8ad84a11b0b2",
 "manifest.json": "3f4f18c23eef358a47b66f84832b6565"
 };
 
