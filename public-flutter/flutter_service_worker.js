b5c41745ea32ffd8d06e5c213e06f4bd3c8f01cc
Andrey Evdokimov <a.evdokimov@2050.digital>
2020-09-28 17:39:50 +0300

    [front] reskin

--- public-flutter/flutter_service_worker.js
+++ public-flutter/flutter_service_worker.js
@@ -12,9 +12,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "ddef918993ef3a0b5fc1e0fceecc5a4a",
-"/": "ddef918993ef3a0b5fc1e0fceecc5a4a",
-"main.dart.js": "589d3a79ec456595afa77be6e736bea5",
+"index.html": "1ac91183b1bcbf8744dbefe42f393cd8",
+"/": "1ac91183b1bcbf8744dbefe42f393cd8",
+"main.dart.js": "f66609d694a822e1eead84cf05c110eb",
 "manifest.json": "3f4f18c23eef358a47b66f84832b6565"
 };
 
