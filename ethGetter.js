281521a3238e269a7e721e051d4cdb96b672e0e4
allive <a.ivanov@2050.digital>
2020-09-29 11:21:10 +0300

    fix parseInt(nowConfirmations)

--- ethGetter.js
+++ ethGetter.js
@@ -112,7 +112,7 @@ async function getDeposit(txHash=null, depositAddress = null, nowConfirmations =
 
 
         //if confirmations qty not reach needed yet - grab it from electrumX 
-        if((nowConfirmations < requiredConfirmations) || nowConfirmations===null || requiredConfirmations===null){
+        if((parseInt(nowConfirmations) < parseInt(requiredConfirmations)) || nowConfirmations===null || requiredConfirmations===null){
             try{
                 let gettingTransaction = (await BitcoinHelpers.Transaction.findAllUnspent(thisDeposit.bitcoinAddress))[0]
                 if(typeof gettingTransaction.transactionID !=='undefined'){
